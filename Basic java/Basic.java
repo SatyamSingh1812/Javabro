2cd3e6d44c3828e9d7fc0db47288741ea9295812
Satyam Singh <63447121+SatyamSingh1812@users.noreply.github.com>
2023-12-16 11:25:31 +0000

    Java Basic

---
+++ Basic java/Basic.java
@@ -0,0 +1,9 @@
+package Basic java;
+
+public class Basic {
+        public static void main(String [] args){
+            System.out.println("Hello world , thank you I got new life.");
+        }
+    }
+    
+}
